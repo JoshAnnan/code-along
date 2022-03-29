b9cd11f54e3153592911b52230de1d3fef4a7af1
JoshAnnan <annanjosh04@gmail.com>
2022-03-29 14:13:10 -0700

    React component 6

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
 import { useEffect, useState } from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
-import writers from "./writers";
-import ProfileCard from "./components/ProfileCard";
-import ProfileForm from "./components/ProfileForm";
+import Axios from "axios";
 
 function App() {
-  const [allProfile, setAllProfile] = useState([
-    {
-      firstName: "Joshua",
-      lastName: "Annan-Takyi",
-      email: "annanjosh04@gmail.com",
-      phone: "+233 024 455 000",
-    },
-  ]);
+  const [posts, setPosts] = useState([]);
 
-  const submit = (profile) => {
-    const arr = allProfile;
-    arr.push();
-    setAllProfile();
-  };
+  useEffect(() => {
+    (async () => {
+      let response = await Axios({
+        method: "GET",
+        url: "https://jsonplaceholder.typicode.com/posts",
+      });
+
+      setPosts(response.data);
+    })();
+  });
 
   return (
-    <div>
-      <h1>Writer Profiles</h1>
-      <div className="container">
-        <ProfileForm submit={submit} />
-        {allProfile.map((writer) => (
-          <ProfileCard key={writer.id} writer={writer} />
-        ))}
+    <div className="app">
+      <h1> Daily Posts </h1>
+      <div>
+        <div className="list">
+          {posts.map((post) => (
+            <div key={post.id} className="post">
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
