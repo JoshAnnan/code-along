673eda842e7ff6860277c2630c0d951880b95fa2
JoshAnnan <annanjosh04@gmail.com>
2022-04-05 14:17:30 -0700

    react-component-9

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
 import NavBar from "./components/NavBar/NavBar";
+import BlogList from "./pages/BlogList";
+import BlogDetail from "./pages/BlogDetail";
 
 const App = () => {
   return (
@@ -14,7 +16,10 @@ const App = () => {
 
         <Route path="about" element={<About />} />
 
-        <Route path="blog" element={<Blog />} />
+        <Route path="blog" element={<Blog />}>
+          <Route path="" element={<BlogList />} />
+          <Route path=":blog" element={<BlogDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,47 +1,7 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
 
-const Blog = () => {
-  return (
-    <section>
-      <h1>Welcome to the Blog Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium
-        esse expedita dolorum hic veritatis error! Laudantium id dolorum modi
-        illo maxime omnis voluptates enim similique praesentium, aut veritatis
-        nam et? Iste at reiciendis voluptas possimus culpa ratione. Veniam
-        corrupti hic sint commodi, id ea, voluptatum facere dolores eaque amet
-        illo nulla dignissimos nobis numquam maxime? Totam qui impedit facilis
-        sint? Impedit voluptates saepe cumque officia veritatis adipisci,
-        architecto consequuntur, ad quas unde ratione. Nemo eos tempore veniam
-        molestias libero repellendus rerum quidem incidunt distinctio, ipsum
-        nobis delectus sint deleniti accusamus.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium
-        esse expedita dolorum hic veritatis error! Laudantium id dolorum modi
-        illo maxime omnis voluptates enim similique praesentium, aut veritatis
-        nam et? Iste at reiciendis voluptas possimus culpa ratione. Veniam
-        corrupti hic sint commodi, id ea, voluptatum facere dolores eaque amet
-        illo nulla dignissimos nobis numquam maxime? Totam qui impedit facilis
-        sint? Impedit voluptates saepe cumque officia veritatis adipisci,
-        architecto consequuntur, ad quas unde ratione. Nemo eos tempore veniam
-        molestias libero repellendus rerum quidem incidunt distinctio, ipsum
-        nobis delectus sint deleniti accusamus.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium
-        esse expedita dolorum hic veritatis error! Laudantium id dolorum modi
-        illo maxime omnis voluptates enim similique praesentium, aut veritatis
-        nam et? Iste at reiciendis voluptas possimus culpa ratione. Veniam
-        corrupti hic sint commodi, id ea, voluptatum facere dolores eaque amet
-        illo nulla dignissimos nobis numquam maxime? Totam qui impedit facilis
-        sint? Impedit voluptates saepe cumque officia veritatis adipisci,
-        architecto consequuntur, ad quas unde ratione. Nemo eos tempore veniam
-        molestias libero repellendus rerum quidem incidunt distinctio, ipsum
-        nobis delectus sint deleniti accusamus.
-      </p>
-    </section>
-  );
-};
+function Blog() {
+  return <Outlet />;
+}
 
 export default Blog;
